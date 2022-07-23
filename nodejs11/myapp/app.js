098429ab8cc219f030893af958152807104e8616
jihopark <jihodayo@gmail.com>
2022-07-23 11:32:22 +0900

    update

--- nodejs11/myapp/app.js
+++ nodejs11/myapp/app.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.json());
+
+app.get('*', function(request, response, next){
+    fs.readdir('./data', function(error, filelist){
+      request.list = filelist;
+      next();
+    });
+  });
 
 app.use('/', require('./html/option'));
+// console.log(__dirname);
+// app.use(express.static(__dirname + '/html/sample5'));
 
 var server = app.listen(3000, () => {
     console.log('app listening on port : 8000');
