daebdf0078facd48e4569515c071c5a834f7f4c6
JoTabPNU <mvmarr0911@pusan.ac.kr>
2022-07-28 16:59:00 +0900

    Update mainArduino_point.ino

--- Arduino/mainArduino_point/mainArduino_point.ino
+++ Arduino/mainArduino_point/mainArduino_point.ino
@@ -1,10 +1,13 @@
-#include <StackArray.h>
 #include <SoftwareSerial.h>
 #include <AccelStepper.h>
 #include <TFMPlus.h>  // Include TFMini Plus Library v1.5.0
 TFMPlus tfmP;         // Create a TFMini Plus object
 #include <ArduinoJson.h>
 #include "WiFiEsp.h"
+
+#define BLUE_PIN 22
+#define GREEN_PIN 24
+#define RED_PIN 26
 
 #define sm1_pin1 2
 #define sm1_pin2 3
@@ -16,14 +19,19 @@ TFMPlus tfmP;         // Create a TFMini Plus object
 #define sm2_pin3 8
 #define sm2_pin4 9
 
-#define aroundInterval 13000  // 한바퀴 도는 시간
+#define aroundInterval 8660  // 한바퀴 도는 시간
 #define LIDARDATASIZE 72      // 한바퀴 동안 측정할 데이터 개수 현재 360 / 72 = 5도 마다 측정
 
 char ssid[] = "AndroidHotspot2409";            // your network SSID (name)
 char pass[] = "12345678";        // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
-char server[] = "13.125.205.44";  // server IP address
-int server_port = 3000;           // server port number
+//char server[] = "13.125.205.44";  // server IP address
+//int server_port = 3000;           // server port number
+
+char server[] = "112.153.143.250";  // nas IP address
+int server_port = 8000;           // nas port number
+//http://112.153.143.250:8000/
+
 
 // Initialize the Ethernet client object
 WiFiEspClient client;
@@ -34,6 +42,7 @@ AccelStepper stepper2(4, sm2_pin1, sm2_pin3, sm2_pin2, sm2_pin4);
 int16_t tfDist = 0;    // Distance to object in centimeters
 unsigned long currentMillis;
 unsigned long previousMillis;
+int start_count;
 
 void setup() {
   Serial.begin(9600);
@@ -42,6 +51,18 @@ void setup() {
   delay(20);               // Give port time to initalize
   tfmP.begin(&Serial2);   // Initialize device library object and...
   // pass device serial port to the object.
+
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+
+  LedBlue();
+  delay(50);
+  LedStop();
+  delay(50);
+  LedBlue();
+  delay(50);
+  LedStop();
 
   // initialize ESP module
   WiFi.init(&Serial1);
@@ -93,113 +114,102 @@ void setup() {
     Serial.println();
   }
   else tfmP.printReply();
-  
+
+  start_count = 0;
   delay(1000);
 }
 
 int cur_x = 0; int cur_y = 0;
+int nt_x = 0; int nt_y = 0;
 int cur_rot = 90;
 
-StackArray<int> st_x;
-StackArray<int> st_y;
-StackArray<unsigned long> st_rot;
-
 void loop() {
-  String jsonstr = ""; // json string 선언
+  String jsonstr = ""; // json string 선언  
   LidarDetectAround(jsonstr); // 반시계방향으로 360도 돌면서 거리 측정 및 좌표로 환산
+  Serial.print("Make Json String :");
+  Serial.println(jsonstr);
   SendDataWeb(jsonstr); // 측정된 좌표 웹에 전송
-    
-  if(st_x.isEmpty()){
-    Serial.println("FINISH!!!");
-    delay(10000);
-  }
-
-  // 다음 측정 위치로 이동을 위한 좌표 및 방위각 설정
-  int nt_x = st_x.pop();
-  int nt_y = st_y.pop();
-  unsigned long nt_rot = st_rot.pop();
-  int nt_dist;
-
-  Serial.print("Rotation : ");
-  Serial.println(360 * nt_rot / aroundInterval - 90);
-
-  // 이동 방위각 회전
-  previousMillis = millis();
-  while(true){
-    currentMillis = millis();
-    if(currentMillis - previousMillis >= nt_rot) break;
-    LeftRound();
-  }
-  Stop();
-  //(360 * (currentMillis - previousMillis) / aroundInterval)
-  cur_rot += 360 * nt_rot / aroundInterval;
-  cur_rot = cur_rot % 360;
-  nt_dist = getDataLidar();
-  Serial.print("nt_dist = ");
-  Serial.println(nt_dist);
-  delay(1000);
-
-  // 이동
-  unsigned long lidarInterval = aroundInterval / LIDARDATASIZE;
-  previousMillis = millis();
-  while(true){
-    currentMillis = millis();
-    if(currentMillis - previousMillis >= lidarInterval){
-      int currentLidar = getDataLidar();
-      if(currentLidar < 50 && currentLidar != 0) break;
-      lidarInterval += lidarInterval;
-    }
-    Go();
-  }
-  Stop();
-  cur_x = int((nt_dist - getDataLidar()) * (cos(radians(cur_rot))));
-  cur_y = int((nt_dist - getDataLidar()) * (sin(radians(cur_rot))));
-
-  // 이동 후 방위각 90도로 다시 돌아옴
-  previousMillis = millis();
-  while(true){
-    currentMillis = millis();
-    if(currentMillis - previousMillis >= nt_rot) break;
-    RightRound();
-  }
+  getDataWeb(); // 다음 위치 받음
+  goNextPoint();
   
-  Serial.print("Current Point [x, y] = ");
-  Serial.print(cur_x);
-  Serial.print(" , ");
-  Serial.println(cur_y);
   delay(2000);
 }
 
-void SendDataWeb(const String& jsonstr){
+void getDataWeb(){
+  LedGreen();
   Serial.println("Starting connection to server...");
-  previousMillis = millis();
   while(true){
-    currentMillis = millis();
     // if you get a connection, report back via serial
     if (client.connect(server, server_port)) {
       Serial.println("Connected to server");
-      
       // Make a HTTP request
-      client.print(F("POST /send HTTP/1.1\r\n"));
-      client.print(F("Content-Type: application/json\r\n"));
-      client.print(F("Host: 13.125.205.44:3000\r\n"));
-      client.print(F("Content-Length: "));
-      client.println(jsonstr.length());
+      client.print(F("GET /default/control HTTP/1.1\r\n"));;
+      client.print(F("Host: 112.153.143.250:8000\r\n"));
+      client.print(F("Connection: close\r\n"));
       client.println();
-      client.println(jsonstr);
-  
-      Serial.println("Post success!!!");
+      delay(500);
       break;
     }
     else {
       // if you couldn't make a connection
-      Serial.println("Connection failed and retry");
-      delay(100);
+      Serial.println("Connection failed");
+      LedRed();
+      delay(500);
     }
-    if(currentMillis - previousMillis >= 2000){
-      Serial.println("Conection failed and end");
-      break;
+  }
+  // if there are incoming bytes available
+  // from the server, read them and print them
+  String res_str = "";
+  while(client.connected()){
+    // There is a client connected. Is there anything to read?
+    while(client.available())
+    {
+       res_str = client.readString();
     }
+    Serial.println();
+    Serial.println("Waiting for more data...");
+    delay(10); // Have a bit of patience...
+  }
+  client.stop(); // 클라이언트 접속 종료
+  // close the connection:
+  Serial.println("Client disconnected");
+  
+  //Serial.println(res_str);  // 응답 받은 데이터
+  // 데이터 중 x, y만 뽑아냄
+  int start_str = res_str.indexOf(F(" x = "));
+  int middle_str = res_str.indexOf(F(" y = "));
+  int end_str = res_str.indexOf(F(" </h1>"));
+  String sub_str1 = res_str.substring(start_str + 5, middle_str);
+  String sub_str2 = res_str.substring(middle_str + 5, end_str);
+  nt_x = sub_str1.toInt();
+  nt_y = sub_str2.toInt();
+  Serial.print("Next Location : ");
+  Serial.print(nt_x);
+  Serial.print(" , ");
+  Serial.println(nt_y);
+}
+
+void SendDataWeb(const String& jsonstr){
+  LedGreen();
+  Serial.println("Starting connection to server...");
+  // if you get a connection, report back via serial
+  if (client.connect(server, server_port)) {
+    Serial.println("Connected to server");
+    
+    // Make a HTTP request
+    client.print(F("POST /default/send HTTP/1.1\r\n"));
+    client.print(F("Content-Type: application/json\r\n"));
+    client.print(F("Host: 12.153.143.250:8000\r\n" ));
+    client.print(F("Content-Length: "));
+    client.println(jsonstr.length());
+    client.println();
+    client.println(jsonstr);
+  }
+  else {
+    // if you couldn't make a connection
+    LedRed();
+    Serial.println("Connection failed");
+    delay(500);
   }
   client.flush();
   client.stop(); // 클라이언트 접속 종료
@@ -216,17 +226,19 @@ int getDataLidar(){
 }
 
 void LidarDetectAround(String& jsonstr){
+  LedBlue();
   unsigned long lidarInterval = aroundInterval / LIDARDATASIZE;
   unsigned long lidarRunMillis = millis();
   
   DynamicJsonDocument doc(1024);
   JsonObject root = doc.to<JsonObject>();
   JsonArray Lidar_Location = root.createNestedArray("Lidar_Location");
-  Lidar_Location[0] = cur_x;
-  Lidar_Location[1] = cur_y;
   JsonArray LidarData_x = root.createNestedArray("LidarData_x");
   JsonArray LidarData_y = root.createNestedArray("LidarData_y");
-  int dataCount = 0; int previousDist = -1;
+  
+  Lidar_Location[0] = cur_x;
+  Lidar_Location[1] = cur_y;
+  int dataCount = 0;
   previousMillis = millis();
   while(true){
     currentMillis = millis();
@@ -238,24 +250,66 @@ void LidarDetectAround(String& jsonstr){
         break;
       }
       int currentDist = getDataLidar();
-      if(previousDist < 0) { previousDist = currentDist; }
-      if(currentDist == 0 || currentDist - previousDist > 2 * previousDist ){
-        st_x.push(cur_x);
-        st_y.push(cur_y);
-        st_rot.push(currentMillis - previousMillis);
-      }
-      previousDist = currentDist;
-      LidarData_x[dataCount] = int(currentDist * (cos(radians(cur_rot + (360 * (currentMillis - previousMillis) / aroundInterval)))));
-      LidarData_y[dataCount] = int(currentDist * (sin(radians(cur_rot + (360 * (currentMillis - previousMillis) / aroundInterval)))));
+      LidarData_x[dataCount] = cur_x + int(currentDist * (cos(radians(cur_rot + (360 * (currentMillis - previousMillis) / aroundInterval)))));
+      LidarData_y[dataCount] = cur_y + int(currentDist * (sin(radians(cur_rot + (360 * (currentMillis - previousMillis) / aroundInterval)))));
       dataCount++;
     }
     LeftRound();
   }
-  Serial.println("==============");
+  Stop();
   serializeJson(doc, jsonstr);
-  Serial.println(jsonstr);
-  Serial.println("==============");
-  return jsonstr;
+}
+
+void goNextPoint(){
+  LedPupple();
+  // 다음 측정 위치로 이동을 위한 방위각 설정
+  nt_x -= cur_x;
+  nt_y -= cur_y;
+
+  float degree = degrees(atan(float(nt_y) / nt_x));
+  unsigned long nt_rot;
+  if(degree < 90){
+    degree += 360;
+  }
+  nt_rot = int((degree - 90) / 360 * aroundInterval);
+  // 이동 방위각 회전
+  previousMillis = millis();
+  while(true){
+    currentMillis = millis();
+    if(currentMillis - previousMillis >= nt_rot) break;
+    LeftRound();
+  }
+  Stop();
+  
+  unsigned long nt_dist = sqrt(pow(nt_x, 2) + pow(nt_y, 2));
+  Serial.print("next_dist = ");
+  Serial.println(nt_dist);
+  delay(500);
+
+  // 이동
+  unsigned long lidarInterval = aroundInterval / LIDARDATASIZE;
+  previousMillis = millis();
+  unsigned long startMillis = previousMillis;
+  while(true){
+    currentMillis = millis();
+    if(currentMillis - previousMillis >= nt_dist * 200){
+      previousMillis = currentMillis;
+      break;
+    }
+    Go();
+  }
+  Stop();
+
+  cur_x += nt_x;
+  cur_y += nt_x;
+
+  // 이동 후 방위각 90도로 다시 돌아옴
+  previousMillis = millis();
+  while(true){
+    currentMillis = millis();
+    if(currentMillis - previousMillis >= nt_rot) break;
+    RightRound();
+  }
 }
 
 void printWifiStatus()
@@ -282,22 +336,48 @@ void Stop(){
 }
 
 void Go(){
-  stepper1.setSpeed(-1000);
-  stepper2.setSpeed(1000);
+  stepper1.setSpeed(1000);
+  stepper2.setSpeed(-1000);
   stepper1.runSpeed();
   stepper2.runSpeed();
 }
 
 void RightRound(){
-  stepper1.setSpeed(500);
-  stepper2.setSpeed(500);
+  stepper1.setSpeed(1000);
+  stepper2.setSpeed(1000);
   stepper1.runSpeed();
   stepper2.runSpeed();
 }
 
 void LeftRound(){
-  stepper1.setSpeed(-500);
-  stepper2.setSpeed(-500);
+  stepper1.setSpeed(-1000);
+  stepper2.setSpeed(-1000);
   stepper1.runSpeed();
   stepper2.runSpeed();
+}
+
+void LedBlue(){
+  digitalWrite(BLUE_PIN, HIGH);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(RED_PIN, LOW);
+}
+void LedGreen(){
+  digitalWrite(BLUE_PIN, LOW);
+  digitalWrite(GREEN_PIN, HIGH);
+  digitalWrite(RED_PIN, LOW);
+}
+void LedRed(){
+  digitalWrite(BLUE_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(RED_PIN, HIGH);
+}
+void LedPupple(){
+  digitalWrite(BLUE_PIN, HIGH);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(RED_PIN, HIGH);
+}
+void LedStop(){
+  digitalWrite(BLUE_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(RED_PIN, LOW);
 }
